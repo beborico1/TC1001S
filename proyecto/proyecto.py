c915561402a4999a2b2c165c58061fe47a49331f
Vrip <vripfashion@hotmail.com>
2022-05-13 18:40:42 -0700

    create new map and fix keyboard functions

--- proyecto/proyecto.py
+++ proyecto/proyecto.py
@@ -1,9 +1,10 @@
-from turtle import * 
 from turtle import *
+ from freegames import floor, vector
 
-comparador = True
+ comparador = True
+ path = Turtle(visible=False)
 
-mapa = [
+ mapa = [
      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -24,10 +25,42 @@ mapa = [
      [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
      [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
      [2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0,
+     0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0,
+     0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0,
+     0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0,
+     0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0,
+     0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0,
+     0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0,
+     0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+     0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0,
+     0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0,
+     0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0,
+     0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0,
+     0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0,
+     0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+     0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+     0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0,
+     0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0,
+     0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+     0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+     0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0,
+     0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0,
+     0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 3, 1, 1, 1, 0, 1, 1, 1, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0,
  ]
 
 
-while comparador == True:
+ def square(x, y):
+     "Draw square using path at (x, y)."
+     path.up()
+     path.goto(x, y)
+     path.down()
+     path.begin_fill()
+
+ while comparador == True:
      comparador2 = True
      print(mapa)
      for x in range(20):
@@ -95,4 +128,93 @@ while comparador == True:
              else:
                  continue
          if break_out_flag:
-            break
+             break 
+     for count in range(4):
+         path.forward(24)
+         path.left(90)
+
+     path.end_fill()
+
+ def world():
+     "Draw world using path."
+     bgcolor('green')
+
+     for index in range(len(mapa)):
+         tile = mapa[index]
+
+         if tile == 1:
+             path.color('grey')
+             x = (index % 25) * 25 - 310
+             y = 290 - (index // 25) * 25
+             square(x, y)
+         elif tile == 2:
+             path.color('red')
+             x = (index % 25) * 25 - 310
+             y = 290 - (index // 25) * 25
+             square(x, y)
+         elif tile == 3:
+             path.color('yellow')
+             x = (index % 25) * 25 - 310
+             y = 290 - (index // 25) * 25
+             square(x, y)
+
+
+ def encontrar(lista):
+     for v in range(625):
+         if lista[v] == 2:
+             return v
+         else: 
+             continue
+
+
+ def arriba():
+     posicion = encontrar(mapa)
+     if (mapa[posicion-25] == 1):
+         mapa[posicion-25] = 2
+         mapa[posicion] = 1
+     elif (mapa[posicion-25] == 3):
+         mapa[posicion-25] = 2
+         mapa[posicion] = 1
+         tracer(False)
+         world() 
+         done()
+
+
+ def izquierda():
+     posicion = encontrar(mapa)
+     if (mapa[posicion-1] == 1):
+         mapa[posicion-1] = 2
+         mapa[posicion] = 1
+     elif (mapa[posicion-1] == 3):
+         mapa[posicion-1] = 2
+         mapa[posicion] = 1
+         tracer(False)
+         world() 
+         done()
+
+
+
+ def abajo():
+     posicion = encontrar(mapa)
+     if (mapa[posicion+25] == 1):
+         mapa[posicion+25] = 2
+         mapa[posicion] = 1
+     elif (mapa[posicion+25] == 3):
+         mapa[posicion+25] = 2
+         mapa[posicion] = 1
+         tracer(False)
+         world() 
+         done()
+
+
+ def derecha():
+     posicion = encontrar(mapa)
+     if (mapa[posicion+1] == 1):
+         mapa[posicion+1] = 2
+         mapa[posicion] = 1
+     elif (mapa[posicion+1] == 3):
+         mapa[posicion+1] = 2
+         mapa[posicion] = 1
+         tracer(False)
+         world() 
+         done()
